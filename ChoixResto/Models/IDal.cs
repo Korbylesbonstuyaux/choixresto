3cddcf911df86bab6b9931ad030326e9d86c1f4c
Korbylesbonstuyaux <korb.softwares@gmail.com>
2016-07-04 22:46:03 +0200

    Add project

---
+++ ChoixResto/Models/IDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.Models
+{
+    public interface IDal : IDisposable
+    {
+        void CreerRestaurant(string nom, string telephone);
+        List<Resto> ObtientTousLesRestaurants();
+    }
+}
